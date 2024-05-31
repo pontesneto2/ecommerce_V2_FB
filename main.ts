39c438f4748f468c26b3383f3d94cd2505947075
Pontes Neto <97975135+pontesneto2@users.noreply.github.com>
2024-05-31 12:29:26 -0300

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,17 @@
+import { enableProdMode } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
+import { appRoutes } from './app/app.routes';
+import { provideRouter } from '@angular/router';
+
+import { environment } from './environments/environment';
+
+if (environment.production) {
+  enableProdMode();
+}
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(appRoutes)
+  ]
+}).catch(err => console.error(err));
